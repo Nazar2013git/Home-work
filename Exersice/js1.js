d34480eaf7ec5edb6b4244fde915b32ce3c6695c
Nazar <nazaryerulanuly@gmail.com>
2024-10-30 09:50:32 +0500

    Example of variables

---
+++ Exersice/js1.js
@@ -0,0 +1,14 @@
+var name = "Nazar";
+
+name = "Abumansur";
+
+name = "Syrym";
+
+const phone = "192.189.10.23";
+
+phone = "123.145.24.19";
+
+document.getElementById("username").innerHTML = name;
+
+
+
