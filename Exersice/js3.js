c3aa579da0e0fb14619e1594036ae80179877f0d
Nazar <nazaryerulanuly@gmail.com>
2024-10-30 10:54:38 +0500

    exersice

---
+++ Exersice/js3.js
@@ -0,0 +1,18 @@
+var name = "Nazar";
+var surname = "Yesenali";
+var age = 11;
+var weight = 35;
+var height = 160;
+var school = 36;
+
+console.log("Name", name);
+console.log("Surname",surname);
+console.log("Age",age);
+console.log("Weight",weight);
+console.log("Height",height);
+console.log("School",school);
+
+
+
+
+
