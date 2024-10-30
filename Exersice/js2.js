079f0a483310d7382184a0cdf9a2b5e3a4db8f9f
Nazar <nazaryerulanuly@gmail.com>
2024-10-30 10:43:25 +0500

    data types

---
+++ Exersice/js2.js
@@ -0,0 +1,16 @@
+var streetName = "Maulenov, 92"; // String
+var currentDay = 30; // Number
+var AmirKeldi = false; // Boolean true = шындық, false = өтірік
+var janaSumka = null; // Бос мән
+var porche = underfined; // Жоқ айнымалы
+
+
+console.log("Көше"),streetName;
+console.log("Күн"),currentDay;
+console.log("АмирКелді",AmirKeldi);
+console.log("жанаСумка",janaSumka);
+console.log("порше",porche);
+
+
+
+
