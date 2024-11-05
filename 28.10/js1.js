399c7ecf96b776c08d7f954ab31089ea0454882d
Nazar <nazaryerulanuly@gmail.com>
2024-11-05 12:18:18 +0500

    2

---
+++ 28.10/js1.js
@@ -0,0 +1,20 @@
+var slicesPerPizza = 8;
+
+var numberOfPizza = 5;
+
+let totalSlices = numberOfPizza * slicesPerPizza;
+
+console.log(totalSlices);
+
+var totalChocolates = 24;
+
+var numberOfFriends = 6;
+
+let chocolatesPerFriend = totalChocolates / numberOfFriends;
+
+console.log(chocolatesPerFriend);
+
+
+
+
+
