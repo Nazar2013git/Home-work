f1ee87143bcd39c7c4a85e0563ace3107cb28b96
Nazar2013git <nazaryerulanuly@gmail.com>
2024-11-11 10:38:08 +0500

    Add files via upload

---
+++ Exercise3.js
@@ -0,0 +1,12 @@
+var a = Number(prompt("a санын жазыңыз")) 
+var b = Number(prompt("b санын жазыңыз")) 
+ 
+if (a > b) { 
+    alert("a  саны  b санынан үлкенірек") 
+} else if ( a < b) { 
+    alert("a саны b санынан кішірек") 
+} else if(a == b) { 
+    alert( "a және b сандары тең" ) 
+} 
+ 
+alert((a + b)/2)
